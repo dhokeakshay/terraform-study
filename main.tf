bd8b73b0b74e660594a703254b2115ceb00aa67b
Akshay <dhoke.akshay15@gmail.com>
2023-09-15 21:04:55 +0530

    "first commit"

--- main.tf
+++ main.tf
@@ -1,20 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
 
 provider "aws" {
   region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = data.aws_ami.ami.id
   instance_type = "t2.micro"
 
   tags = {
---
+++ variable.tf
@@ -0,0 +1,4 @@
+data "aws_ami" "ami" {
+  type = string
+  default = "ami-830c94e3"
+}
