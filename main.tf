ae746952808f34ddf698d3da6070668aeb0e5a88
Akshay <dhoke.akshay15@gmail.com>
2023-09-16 11:38:46 +0530

    "first commit"

--- main.tf
+++ main.tf
@@ -1,14 +1,10 @@
 
-provider "aws" {
-  region = "us-west-2"
-}
-
-resource "aws_instance" "app_server" {
+resource "aws_instance" "web_server" {
   ami           = data.aws_ami.ami.id
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "WebServerInstance"
   }
 }
 
--- variable.tf
+++ variable.tf
@@ -1,3 +1,8 @@
+
+provider "aws" {
+  region = "us-west-2"
+}
+
 data "aws_ami" "ami" {
   type = string
   default = "ami-830c94e3"
