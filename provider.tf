072f8687d0fc39f56555c2787c1dbfd532a5d53b
Akshay <dhoke.akshay15@gmail.com>
2023-09-16 18:49:08 +0530

    "updated file"

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ data "aws_ami" "amazon_linux" {
   owners      = ["137112412989"]
 
   filter{
-    name = "AmiName"
+    name = "name"
     values = ["al2023-ami-2023.1.20230912.0-kernel-6.1-x86_64"]
   }
   filter{
