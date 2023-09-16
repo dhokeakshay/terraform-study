c6ad41e5dbfdade9975c8135266bada3c43a9578
Akshay <dhoke.akshay15@gmail.com>
2023-09-16 12:07:26 +0530

    "first commit"

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "web_server" {
-  ami           = data.aws_ami.ami.id
+  ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
--- variable.tf
+++ variable.tf
@@ -3,7 +3,3 @@ provider "aws" {
   region = "us-west-2"
 }
 
-data "aws_ami" "ami" {
-  types= string
-  default = "ami-830c94e3"
-}
