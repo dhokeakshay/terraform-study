7660e6bb410f62f01d94b2bf951336d98434c9f6
Akshay <dhoke.akshay15@gmail.com>
2023-09-21 19:08:04 +0530

    "attached ebs volume"

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 
 resource "aws_instance" "web_server" {
+  count = 3
   ami           = "ami-0b825ad86ddcfb907"
   instance_type = "t2.micro"
 
   tags = {
-    Name = var.instance_name
+    Name = var.instance_name[3]
   }
 }
 resource "aws_ebs_volume" "ebs_volume" {
